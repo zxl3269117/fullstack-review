e3e7d8c8da2524573c18b500e5bda1511363535e
Ziqian Li <liziqian1992@gmail.com>
2022-06-01 19:11:14 -0700

    Complete getReposByUsername

--- server/index.js
+++ server/index.js
@@ -6,15 +6,29 @@ const data = require('../data.json');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.post('/repos', function (req, res) {
-  console.log(req.body);
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+  console.log(req.body.term);
 
-  res.status(201).json(data);
+  var username = req.body.term;
+  github.getReposByUsername(username)
+    .then(response => {
+      console.log(response.data);
+      // sort data
+
+      // return sorted data
+
+    })
+    .then(sortedRepos => {
+      // add sortedRepos to the DB
+      // return sortedRepos.splice(0,  24)
+    })
+    .then(topRepos => {
+      // send res with topRepos
+      res.status(201).json(topRepos);
+    })
+    .catch( err => console.log(err) )
 });
 
 app.get('/repos', function (req, res) {
